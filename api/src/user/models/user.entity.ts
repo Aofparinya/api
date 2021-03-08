330ab06a76815d197f8fff7f1865dcb96b0f0b0a
Aofparinya <sa.aofparinya@gmail.com>
2021-03-08 23:07:18 +0700

    login

---
+++ api/src/user/models/user.entity.ts
@@ -0,0 +1,30 @@
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class UserEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column({ unique:true })
+    username: string;
+
+    @Column()
+    career: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @BeforeInsert()
+    emailToLowerCase() {
+        this.email = this.email.toLowerCase() ;
+    }
+
+
+}
---
+++ api/src/user/models/user.interface.ts
@@ -0,0 +1,10 @@
+export interface User { 
+
+    id?: number;
+    name?: string;
+    username?: string;
+    career?: string;
+    email?: string;
+    password?: string;
+
+}
